6cf13bd93731640b14a1c329a3e949d6db0e07cb
Daniel-kim800 <55107493+Daniel-kim800@users.noreply.github.com>
2020-04-06 19:35:15 -0500

    Delete Sum.java

--- Sum.java
+++
@@ -1,9 +0,0 @@
-public class Sum {
-    public static void main(String[] args) {
-
-        int num1 = 5, num2 = 15;
-        int sum = num1 + num2;
-
-        System.out.println("Sum of these numbers: "+sum);
-    }
-}
